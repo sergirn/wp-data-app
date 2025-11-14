50ec465958a7297383c3b98b27ca3677204044eb
Sergi Rojas Navarro <sergi.rojas@applusglobal.com>
2025-11-14 16:36:02 +0100

    test para instalar la app

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,25 @@
+import withPWA from "next-pwa";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-}
+	eslint: {
+		ignoreDuringBuilds: true
+	},
+	typescript: {
+		ignoreBuildErrors: true
+	},
+	images: {
+		unoptimized: true
+	}
+};
 
-export default nextConfig
+// Exportamos configuración combinada con soporte PWA
+export default withPWA({
+	dest: "public",
+	register: true,
+	skipWaiting: true,
+
+	// Muy importante:
+	// No activar PWA en modo dev, solo en producción
+	disable: process.env.NODE_ENV === "development"
+})(nextConfig);
